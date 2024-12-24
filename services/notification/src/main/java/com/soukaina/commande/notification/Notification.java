07d1f08f9ce7727d5f1efb5977a09af5191980af
Soukaina Jabri <soukaina.jabri333@gmail.com>
2024-12-24 12:10:47 +0100

    first commit

--- services/notification/src/main/java/com/soukaina/commande/notification/Notification.java
+++ services/notification/src/main/java/com/soukaina/commande/notification/Notification.java
@@ -1,7 +1,6 @@
 package com.soukaina.commande.notification;
 
 import com.soukaina.commande.kafka.order.OrderConfirmation;
-import com.soukaina.commande.kafka.payment.PaymentConfirmation;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
